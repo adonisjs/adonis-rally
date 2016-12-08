11e8bbff305e380bb50d0b2f071d1703b5097151
Harminder Virk <virk.officials@gmail.com>
2016-12-08 19:42:13 +0530

    fix(migrations): use refresh instead of posttest

--- database/migrations/1469451312454_Answers.js
+++ database/migrations/1469451312454_Answers.js
@@ -7,8 +7,8 @@ class AnswersSchema extends Schema {
   up () {
     this.create('answers', (table) => {
       table.increments()
-      table.integer('question_id').unsigned()
-      table.integer('user_id').unsigned()
+      table.integer('question_id').references('id').inTable('questions')
+      table.integer('user_id').references('id').inTable('users')
       table.boolean('best_answer').defaultTo(0)
       table.text('body', 'longtext').notNullable()
       table.timestamps()
