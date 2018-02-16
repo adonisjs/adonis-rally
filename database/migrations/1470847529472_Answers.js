715a22c541042e63fbe767478f81d581cc5b86e0
Bhojendra Shah Rauniyar <heartrelative2007@gmail.com>
2018-02-17 04:03:08 +0545

    fix(migration): add unsigned attribute to foreign keys

--- database/migrations/1470847529472_Answers.js
+++ database/migrations/1470847529472_Answers.js
@@ -7,8 +7,8 @@ class AnswersSchema extends Schema {
   up () {
     this.create('answers', (table) => {
       table.increments()
-      table.integer('question_id').references('id').inTable('questions')
-      table.integer('user_id').references('id').inTable('users')
+      table.integer('question_id').unsigned().references('id').inTable('questions')
+      table.integer('user_id').unsigned().references('id').inTable('users')
       table.boolean('best_answer').defaultTo(0)
       table.text('body', 'longtext').notNullable()
       table.timestamps()
